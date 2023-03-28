c192d6d6d23b81d25ff810e60f5d6b0f7879c4ef
Bogdan <bogdan.burcea22@my.northampton.ac.uk>
2023-03-28 23:51:27 +0100

    start button hide when click + added defer

--- game.js
+++ game.js
@@ -5,72 +5,69 @@ var rightPressed = false;
 var lastPressed = false;
 
 function keyup(event) {
-	var player = document.getElementById('player');
+	var player = document.getElementById("player");
 	if (event.keyCode == 37) {
 		leftPressed = false;
-		lastPressed = 'left';
+		lastPressed = "left";
 	}
 	if (event.keyCode == 39) {
 		rightPressed = false;
-		lastPressed = 'right';
+		lastPressed = "right";
 	}
 	if (event.keyCode == 38) {
 		upPressed = false;
-		lastPressed = 'up';
+		lastPressed = "up";
 	}
 	if (event.keyCode == 40) {
 		downPressed = false;
-		lastPressed = 'down';
+		lastPressed = "down";
 	}
 
-	player.className = 'character stand ' + lastPressed;
+	player.className = "character stand " + lastPressed;
 	console.log(player.className);
 }
 
-
 function move() {
-	var player = document.getElementById('player');
+	var player = document.getElementById("player");
 	var positionLeft = player.offsetLeft;
 	var positionTop = player.offsetTop;
 	if (downPressed) {
 		var newTop = positionTop + 2;
 
-		player.style.top = newTop + 'px';
+		player.style.top = newTop + "px";
 
 		if (leftPressed == false) {
 			if (rightPressed == false) {
-				player.className = 'character walk down';
+				player.className = "character walk down";
 			}
 		}
 	}
 	if (upPressed) {
 		var newTop = positionTop - 3;
 
-		player.style.top = newTop + 'px';
+		player.style.top = newTop + "px";
 
 		if (leftPressed == false) {
 			if (rightPressed == false) {
-				player.className = 'character walk up';
+				player.className = "character walk up";
 			}
 		}
 	}
 	if (leftPressed) {
 		var newLeft = positionLeft - 1;
 
-		player.style.left = newLeft + 'px';
+		player.style.left = newLeft + "px";
 
-		player.className = 'character walk left';
+		player.className = "character walk left";
 	}
 	if (rightPressed) {
 		var newLeft = positionLeft + 1;
 
-		player.style.left = newLeft + 'px';
+		player.style.left = newLeft + "px";
 
-		player.className = 'character walk right';
+		player.className = "character walk right";
 	}
-
 }
-
 
 function keydown(event) {
 	if (event.keyCode == 37) {
@@ -87,16 +84,38 @@ function keydown(event) {
 	}
 }
 
-
-
 function myLoadFunction() {
 	timeout = setInterval(move, 10);
-	document.addEventListener('keydown', keydown);
-	document.addEventListener('keyup', keyup);
+	document.addEventListener("keydown", keydown);
+	document.addEventListener("keyup", keyup);
 }
+
+
+let startButton = document.getElementsByClassName('start');
 
 function startGame() {
-	
+	startButton[0].style.display = "none";
+	console.log("test")
 }
 
-document.addEventListener('DOMContentLoaded', myLoadFunction);
+
+
+if (startButton[0]) {
+	startButton[0].addEventListener('click', startGame);
+}
+
+// initial code that was not working
+// let startButton = document.getElementsByClassName('start');
+
+// function startGame() {
+// 	startButton[0].style.display = "none";
+// 	console.log("test")
+// }
+// startButton.addEventListener('click', startGame);
+// 
+// link1: https://stackoverflow.com/questions/26107125/cannot-read-property-addeventlistener-of-null
+// bug2: debugged by using console.log(startButton) = index 0 with value
+// bug 3: had to add defer to game.html as the script was starting before dom loading
+
+
+document.addEventListener("DOMContentLoaded", myLoadFunction);
