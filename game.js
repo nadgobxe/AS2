8c32697a815f64dcc6835d0c73148d9e8b5d7493
Bogdan <bogdan.burcea22@my.northampton.ac.uk>
2023-03-31 00:08:46 +0100

    not pass through cactus 10% implemented

--- game.js
+++ game.js
@@ -103,23 +103,63 @@ function tankStart() {  // tank function
 	}
   };
 
+	var selectCactus = document.querySelectorAll("body >div.cactus");
+	var playerNew = document.querySelector("#player");
 
+	
 function startGame() {
 	startButton[0].style.display = "none"; // hide start div
 	tankStart() // call tankstart function
-	console.log("test")
+	console.log("test StartGame")
 }
 
+function loadStartGame() { // load startGame()
+	startButton[0].addEventListener('click', startGame);
+}
+
+function preventCollision(player, cactus) {
+	setInterval(function() {
+	  var playerRect = player.getBoundingClientRect();
+	  var cactusRect = cactus.getBoundingClientRect();
+	//   console.log(player.offsetWidth);
+	//   console.log(cactusRect.left);
+  
+	  if (playerRect.x + playerRect.width >= cactusRect.x &&
+		  playerRect.x <= cactusRect.x + cactusRect.width  &&
+		  playerRect.y + playerRect.height >= cactusRect.y &&
+		  playerRect.y <= cactusRect.y + cactusRect.height ) {
+		console.log("collision detected!");
+		console.log(playerRect.width);
+		// handle collision here, for example:
+		// player.style.display = "none";
+
+		if (playerRect.x + playerRect.width >= cactusRect.x) {
+            player.style.left = (playerRect.x - 1) + "px";
+			console.log(playerRect.x)
+        }
+        if (playerRect.x <= cactusRect.x + cactusRect.width) {
+            player.style.left = (playerRect.x + 1) + "px";
+        }
+        if (playerRect.y + playerRect.height >= cactusRect.y) {
+            player.style.top = (playerRect.y - 1) + "px";
+        }
+        if (playerRect.y <= cactusRect.y + cactusRect.height) {
+            player.style.top = (playerRect.y + 1) + "px";
+        }
+	  }
+	}, 10); // check for collision every 10 milliseconds
+  }
+
+  preventCollision(playerNew, selectCactus[0]);
+
+// Testing Area
 var numberOfTanks = document.querySelectorAll('body > div.tank');
 console.log(numberOfTanks);
 var randomTankFire = Math.floor(Math.random() * numberOfTanks.length);
 console.log(randomTankFire);
 var selectTank = numberOfTanks[randomTankFire];
 console.log(selectTank);
-
-if (startButton[0]) { // if startButton[ with the index of 0] = true please run the below
-	startButton[0].addEventListener('click', startGame);
-}
+console.log(playerNew);
 
 
 // initial code that was not working
@@ -137,3 +177,5 @@ if (startButton[0]) { // if startButton[ with the index of 0] = true please run 
 
 
 document.addEventListener("DOMContentLoaded", myLoadFunction);
+document.addEventListener("DOMContentLoaded", loadStartGame); // Loads LoadStartGame()
+document.addEventListener("DOMContentLoaded", detectCollision);
