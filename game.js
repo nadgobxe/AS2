0d7c5262d59bca8fce456c0bfc7915fcf0327262
Bogdan <bogdan.burcea22@my.northampton.ac.uk>
2023-04-30 13:32:13 +0100

    hide_startbar

--- game.js
+++ game.js
@@ -6,6 +6,11 @@ var rightPressed = false;
 var lastPressed = false;
 var timeout = 0;
 
+var selectStartBar = document.getElementsByClassName('start')[0]; //select Start Bar
+var selectTanks = document.getElementsByClassName('tanks'); // select tanks
+
+
+
 function keyup(event) {
 	var player = document.getElementById('player');
 	if (event.keyCode == 37) {
@@ -70,10 +75,37 @@ function keydown(event) {
 	}
 }
 
+
+
+function loopItems(param, loopFunction) { // reusable function param = item select via for loop (tank, bomb, explosion etc) and loopFunction is the traget function like (randomTanks)
+	for (var i = 0; i < param.length; i++) {
+		loopFunction(param[i]);
+	}
+}
+
+
+function randomTanksSpawn(tank) {
+	var randomize = Math.floor(Math.random() * window.innerHeight); // generate random number
+	tank.style.display = "block";
+	tank.style.top = randomize + "px";
+	console.log("Value of the tank is" + tank);
+}
+
 function myLoadFunction() {
+
 	timeout = setInterval(move, 10);
 	document.addEventListener('keydown', keydown);
 	document.addEventListener('keyup', keyup);
+
+
+	selectStartBar.addEventListener('click', startGame);
+}
+
+function startGame() {
+	// tank starting to fire
+	selectStartBar.style.display = "none";
+	selectTanks.style.display = "none";
+	randomTanksSpawn(selectTanks[0]);
 }
 
 document.addEventListener('DOMContentLoaded', myLoadFunction);
