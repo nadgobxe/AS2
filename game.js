8ad6126600d2bd2f584205faf3aea51e87f4b6b3
Bogdan <bogdan.burcea22@my.northampton.ac.uk>
2023-05-03 19:48:49 +0100

    random spawn tanks.

--- game.js
+++ game.js
@@ -4,6 +4,12 @@ var leftPressed = false;
 var rightPressed = false;
 var lastPressed = false;
 
+var startBar = document.getElementsByClassName('start')[0];
+var tanks = document.getElementsByClassName('tank');
+
+var timeoutTanks = 0;
+var timeout = 0;
+
 function keyup(event) {
 	var player = document.getElementById('player');
 	if (event.keyCode == 37) {
@@ -85,13 +91,51 @@ function keydown(event) {
 		downPressed = true;
 	}
 }
+// remove start bar
+function removeStartBar() {
+	startBar.style.display = "none";
 
+	//show tanks
+	for (var i = 0; i < tanks.length; i++) {
+		tanks[i].style.display = "block";
+	}
+}
+startBar.addEventListener('click', removeStartBar);
+//end 
+
+// spawn randomly tanks
+function spawnTanks() {
+	var windowHeight = window.innerHeight;
+	for (var i = 0; i < tanks.length; i++) {
+		randomPointY = (Math.floor(Math.random() * windowHeight));
+		console.log("Value of WindowHeight is" + tanks[i].style.top);
+		tanks[i].style.top = randomPointY + 68 + "px";
+
+		var tankTopValue = tanks[i].style.top;
+		var parseTankTopValue = parseInt(tankTopValue);
+		console.log(parseTankTopValue);
+
+		if (windowHeight < parseTankTopValue + 68) { //avoid tank going off from the canvas
+			console.log("Value of WindowHeight is -<<<<<" + windowHeight);
+			tanks[i].style.top = randomPointY - 68 + "px";
+		}
+	}
+
+
+}
+// end
 
 function myLoadFunction() {
+	timeoutTanks = setInterval(spawnTanks, 1000);
 	timeout = setInterval(move, 10);
 	document.addEventListener('keydown', keydown);
 	document.addEventListener('keyup', keyup);
-}
 
+	//hide tanks
+	for (var i = 0; i < tanks.length; i++) {
+		tanks[i].style.display = "none";
+	}
+
+}
 
 document.addEventListener('DOMContentLoaded', myLoadFunction);
