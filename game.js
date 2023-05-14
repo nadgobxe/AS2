f0c594679ed707f83d494ac7cb44a1c2b670e49a
Bogdan <bogdan.burcea22@my.northampton.ac.uk>
2023-05-14 18:43:48 +0100

    bomb added

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ var rightPressed = false;
 var lastPressed = false;
 
 var selectStart = document.getElementsByClassName('start')[0];
+var tanks = document.getElementsByClassName('tank');
 
 function keyup(event) {
 	var player = document.getElementById('player');
@@ -88,15 +89,50 @@ function keydown(event) {
 	}
 }
 
+function tanksSpawn() {
+	for (var i = 0; i < tanks.length; i++) {
+		randomPointY = (Math.floor(Math.random() * window.innerHeight));
+		// console.log("Value of WindowHeight is" + tanks[i].style.top);
+		tanks[i].style.top = randomPointY + 68 + "px";
+		var tankTopValue = tanks[i].style.top;
+		var parseTankTopValue = parseInt(tankTopValue);
+		// console.log(parseTankTopValue);
+		if (window.innerHeight < parseTankTopValue + 68) { //avoid tank going off from the canvas
+			// console.log("Value of WindowHeight is -<<<<<" + windowHeight);
+			tanks[i].style.top = randomPointY - 68 + "px";
+		}
+		var bomb = addBomb();
+
+		bomb.style.left = tanks[i].offsetLeft + 'px';
+		bomb.style.top = tanks[i].offsetTop + 'px';
+	}
+}
+
+function addBomb() {
+	var bomb = document.createElement('div');
+	bomb.classList = 'bomb';
+	document.body.appendChild(bomb);
+
+	return bomb;
+}
+
 function startGame() {
 	selectStart.style.display = 'none';
 	timeout = setInterval(move, 10);
 	document.addEventListener('keydown', keydown);
 	document.addEventListener('keyup', keyup);
+	timeoutTanks = setInterval(tanksSpawn, 2500);
+	for (var i = 0; i < tanks.length; i++) {
+		tanks[i].style.display = "block";
+	}
+
 }
 
 function myLoadFunction() {
-	
+	selectStart.addEventListener('click', startGame);
+	for (var i = 0; i < tanks.length; i++) {
+		tanks[i].style.display = "none";
+	}
 }
 
 document.addEventListener('DOMContentLoaded', myLoadFunction);
