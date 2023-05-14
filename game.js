d7256193f8ee3e26651f6ad36c925ecd2a9d4de1
Bogdan <bogdan.burcea22@my.northampton.ac.uk>
2023-05-14 18:19:07 +0100

    final_shelf_version

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ var downPressed = false;
 var leftPressed = false;
 var rightPressed = false;
 var lastPressed = false;
+var spaceBarPressed = false;
 
 var startBar = document.getElementsByClassName('start')[0];
 var tanks = document.getElementsByClassName('tank');
@@ -12,6 +13,7 @@ var timeoutTanks = 0;
 var timeout = 0;
 var timeoutBomb = 0;
 var timeoutRandomExplode = 0;
+var timeoutArrow = 0;
 var conditionTrigger = 0;
 
 var score = 0;
@@ -23,11 +25,17 @@ var directions = 0;
 var wayBomb = [-0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75];
 var nameS = [];
 var scoreS = [];
+
+var player = document.getElementById('player');
+var weapon = document.getElementsByClassName('weapon')[0];
+var windowHeight = window.innerHeight;
+var windowWidth = window.innerWidth;
+
 // localStorage.clear();
 
 
 function keyup(event) {
-	var player = document.getElementById('player');
+
 	if (event.keyCode == 37) {
 		leftPressed = false;
 		lastPressed = 'left';
@@ -44,25 +52,45 @@ function keyup(event) {
 		downPressed = false;
 		lastPressed = 'down';
 	}
+
+	if (event.keyCode == 32) {
+		spaceBarPressed = false;
+		removeEventDom();
+		setTimeout(function () {
+			weapon.style.display = 'none';
+			weapon.classList.remove('fire');
+			activateEvenDom();
+		}, 500)
+	}
 	player.className = 'character stand ' + lastPressed;
 }
 function move() {
-	var player = document.getElementById('player');
+
 	var positionLeft = player.offsetLeft;
 	var positionTop = player.offsetTop;
+	var positionBottom = positionTop + player.offsetHeight;
+	var tankLeft = tanks[0].offsetLeft;
+	var positionRight = positionLeft + player.offsetWidth;
+
+	// console.log("Left" + positionLeft);
+	// // console.log("Top" + positionTop);
+	// // console.log("Right" + positionRight);
+	// // console.log("Bottom" + positionBottom);
+	// console.log(windowWidth);
+	// console.log(tankLeft);
+
 	if (downPressed == true) {
 		var newTop = positionTop + 1;
 		var element = document.elementFromPoint(positionLeft, newTop + 46); /* Detect position X/Y axis and return the top element at the specified coordinates. In our case, we would like to find when we collide with the 'cactus' class. To do that, we have to check for element.classList.contains('cactus') */
-		// console.log("Value of Element is :" + element.className);
-		if (element.classList.contains('cactus') == false && element.classList.contains('tank') == false) {
+		if (element.classList.contains('cactus') == false && element.classList.contains('tank') == false && positionBottom < (windowHeight - 5)) {
 			player.style.top = newTop + 'px';
-		}
+		}  
 		player.className = 'character walk down';
 	}
 	if (upPressed == true) {
 		var newTop = positionTop - 1;
 		var element = document.elementFromPoint(positionLeft, newTop);
-		if (element.classList.contains('cactus') == false && element.classList.contains('tank') == false) {
+		if (element.classList.contains('cactus') == false && element.classList.contains('tank') == false && positionTop > 5) {
 			player.style.top = newTop + 'px';
 		}
 		player.className = 'character walk up';
@@ -70,7 +98,7 @@ function move() {
 	if (leftPressed == true) {
 		var newLeft = positionLeft - 1;
 		var element = document.elementFromPoint(newLeft, positionTop);
-		if (element.classList.contains('cactus') == false && element.classList.contains('tank') == false) {
+		if (element.classList.contains('cactus') == false && element.classList.contains('tank') == false && positionLeft > 5) {
 			player.style.left = newLeft + 'px';
 		}
 		player.className = 'character walk left';
@@ -78,10 +106,34 @@ function move() {
 	if (rightPressed == true) {
 		var newLeft = positionLeft + 1;
 		var element = document.elementFromPoint(newLeft + 32, positionTop);
-		if (element.classList.contains('cactus') == false && element.classList.contains('tank') == false) {
+		if (element.classList.contains('cactus') == false && element.classList.contains('tank') == false && positionRight < (tankLeft - 10)) {
 			player.style.left = newLeft + 'px';
 		}
+
 		player.className = 'character walk right';
+	}
+
+	if (spaceBarPressed) {
+
+		weapon.style.display = 'block';
+		addArrow();
+		timeoutArrow = setInterval(moveArrow, 10);
+
+		if (!downPressed || !upPressed || !leftPressed || !rightPressed) {
+
+			player.className = 'character stand ' + lastPressed + ' fire';
+		}
+
+		if (downPressed || upPressed || leftPressed || rightPressed) {
+			player.className = 'character fire walk ' + lastPressed;
+		}
+
+		removeEventDom();
+		setTimeout(function () {
+			weapon.style.display = 'none';
+			weapon.classList.remove('fire');
+			activateEvenDom();
+		}, 500)
 	}
 }
 function keydown(event) {
@@ -97,13 +149,93 @@ function keydown(event) {
 	if (event.keyCode == 40) {
 		downPressed = true;
 	}
+
+	if (event.keyCode == 32) {
+		spaceBarPressed = true;
+	}
 }
+
+
+//explode arrow
+function explodeArrow() {
+	var arrows = document.getElementsByClassName('arrow');
+
+	for (var i = 0; i < arrows.length; i++) {
+		var currentArrow = arrows[i];
+		var top = currentArrow.offsetTop;
+		var left = currentArrow.offsetLeft;
+		var right = left + 32; // Move the declaration here
+		var bottom = top + 10;
+
+		//   console.log("left is " + left);
+		//   console.log("right value is " + right);
+
+		var element = document.elementFromPoint(right, top);
+		var elementReverse = document.elementFromPoint(left, bottom);
+		if (element.classList.contains('bomb') || element.classList.contains('tank') || elementReverse.classList.contains('bomb') || elementReverse.classList.contains('tank')) {
+			explodeBomb(currentArrow);
+			currentArrow.remove();
+			clearInterval(timeoutArrow);
+			continue; // Skip the remaining code and proceed to the next arrow
+		}
+		var windowWidth = window.innerWidth;
+		if (top <= 0 || bottom >= (window.innerHeight - 1) || left <= 0 || right >= (windowWidth - 1)) {
+			explodeBomb(currentArrow);
+			currentArrow.remove();
+		}
+	}
+}
+
+// move arrow
+function moveArrow() {
+	var arrows = document.getElementsByClassName('arrow');
+
+	for (var i = 0; i < arrows.length; i++) {
+		var currentArrow = arrows[i];
+		var arrowSpeed = 1;
+		var positionL = currentArrow.offsetLeft;
+		var positionT = currentArrow.offsetTop;
+
+		currentArrow.style.left = positionL + arrowSpeed + 'px';
+
+		// if (leftFire) {
+		// 	currentArrow.style.left = positionL - arrowSpeed + 'px';
+		// }
+
+		// if (rightFire) {
+		// 	currentArrow.style.left = positionL + arrowSpeed + 'px';
+		// }
+
+		// if (upFire) {
+		// 	currentArrow.style.top = positionT - arrowSpeed + 'px';
+		// }	
+
+		// if (downFire) {
+		// 	currentArrow.style.top = positionT + arrowSpeed + 'px';
+		// }	
+	}
+
+	explodeArrow();
+}
+
+//add Arrow
+function addArrow() {
+	var arrow = document.createElement('div');
+	arrow.className = 'arrow';
+	arrow.style.top = player.offsetTop + 'px';
+	arrow.style.left = player.offsetLeft + 'px';
+	arrow.style.position = 'absolute';
+	document.body.appendChild(arrow);
+	
+}
+
+
 // call leaderboard
 function callLeaderboard() {
 	var elLeader = document.createElement('button');
 	var hud = document.getElementsByClassName('hud')[0];
 	hud.appendChild(elLeader);
-	elLeader.classList.add('leaderboard');
+	elLeader.classList.add('leaderboardtop');
 	elLeader.style.position = 'absolute';
 	elLeader.style.left = '50%';
 	elLeader.style.backgroundColor = '#ccc';
@@ -118,7 +250,6 @@ function callLeaderboard() {
 	elLeader.innerHTML = 'Leaderboard';
 	elLeader.addEventListener('click', displayLeaderboard);
 }
-callLeaderboard();
 
 // display leaderboard
 function displayLeaderboard() {
@@ -258,7 +389,13 @@ function createForm() {
 
 		nameS = document.getElementById('name').value;
 		scoreS = document.getElementById('score').value;
-		
+
+		var storeScoreLocal = [];
+		var storeNameLocal = [];
+
+		storeScoreLocal.push(scoreS);
+		storeNameLocal.push(nameS);
+
 		localStorage.setItem('name', JSON.stringify(nameS));
 		localStorage.setItem('score', JSON.stringify(scoreS));
 
@@ -313,7 +450,7 @@ function removeEventDom() { //bug fix when restart stoping the player going on t
 	downPressed = false;
 	leftPressed = false;
 	rightPressed = false;
-	lastPressed = false;
+	spaceBarPressed = false;
 }
 function activateEvenDom() { //reinstante the movement ==============================================
 	document.addEventListener('keyup', keyup);
@@ -340,7 +477,7 @@ function removeLife() {
 	var li = healthBar.getElementsByTagName('li');
 	if (li.length > 1) {
 		li[0].remove();
-		player.className = 'character ' + lastPressed + ' hit'; //hit feature
+		player.className = 'character hit ' + lastPressed; //hit feature
 		removeEventDom(); // remove keyCode event listener
 		setTimeout(activateEvenDom, 2000); // start walking	
 		// player flickers when hit by bomb ====================================
@@ -378,16 +515,11 @@ function bombClearOut() {
 // end ==============================================================================================
 // randomPoint ======================================================================================
 function randomExplode() {
-	var windowWidth = window.innerWidth; //get canvas total width
-	var windowHeight = window.innerHeight; //get canvas total height
-	console.log("Height is:" + windowHeight);
+
 	var retriveTank = document.getElementsByClassName('tank')[0];
 	var offsetTankLeft = retriveTank.offsetLeft;
 	var canvasLength = windowWidth - (offsetTankLeft / 2); // value of remaining distance from tank to left end of the canvas
 	conditionTrigger = Math.floor(canvasLength * Math.random()); // modify conditionTrigger value in the global scope
-	console.log(conditionTrigger);
-	console.log("WW:" + windowWidth);
-	console.log("OT:" + offsetTankLeft)
 }
 randomExplode(); // call function first time
 // end ==============================================================================================
@@ -408,6 +540,7 @@ function gameOver() {
 	restartButton.style.position = 'absolute';
 	gameOver.innerHTML = "Play Again?";
 
+	callLeaderboard();
 
 	var gameOverMessage = document.createElement('div');
 	document.body.appendChild(gameOverMessage);
@@ -445,7 +578,7 @@ function gameOver() {
 	createForm();
 }
 
-// restart game =====================================================================================
+// restart game
 function restartGame() {
 	var gameOver = document.getElementsByClassName('gameover')[0];
 	gameOver.classList.add('start');
@@ -458,9 +591,13 @@ function restartGame() {
 	removeGameOver.classList.remove('gameover-message');
 	removeGameOver.remove();
 
+	var selectLeaderboard = document.getElementsByClassName('leaderboardtop')[0];
+	selectLeaderboard.style.display = 'none';
+
 	var selectForm = document.getElementsByTagName('form')[0]; // get input form
 	var parentOfForm = selectForm.parentNode;
 	parentOfForm.removeChild(selectForm);
+
 
 	var player = document.getElementById('player'); // restore player position and class
 	player.className = 'character stand';
@@ -473,6 +610,8 @@ function restartGame() {
 	downPressed = false;
 	leftPressed = false;
 	rightPressed = false;
+	lastPressed = false;
+	spaceBarPressed = false;
 	lastPressed = false;
 	var health = document.getElementsByClassName('health')[0];
 	for (var i = 0; i < 3; i++) {
@@ -488,10 +627,12 @@ function restartGame() {
 	hiddenScore = 0;
 	addLifeStore = 0;
 	startGame();
+	clearInterval(timeout);
+	clearInterval(timeoutArrow);
 }
-// end ==============================================================================================
+// end
 
-// dead player ======================================================================================
+// dead player
 function deadPlayer() {
 	var player = document.getElementById('player');
 	player.className = 'character dead';
@@ -501,8 +642,8 @@ function deadPlayer() {
 	clearInterval(timeoutTanks);
 	clearInterval(timeout);
 }
-// end ==============================================================================================
-// explode bombs ====================================================================================
+// end 
+// explode bombs
 function explodeBomb(bomb) {
 	var explosion = document.createElement('div');
 	explosion.classList.add('explosion');
@@ -515,45 +656,64 @@ function explodeBomb(bomb) {
 	}
 	setTimeout(explosionOff, 1000) // bomb control - 3rd part sets explosion off after 1 sec
 }
-// end ==============================================================================================
-// move bombs =======================================================================================
+// end
+// move bombs 
 function moveBomb() {
 	var bombs = document.getElementsByClassName('bomb');
+	var arrow = document.getElementsByClassName('arrow');
 	for (var i = 0; i < bombs.length; i++) {
 		var elBomb = bombs[i];
 		var bombLeft = elBomb.offsetLeft;
 		var bombTop = elBomb.offsetTop;
 		var bombRight = elBomb.offsetLeft + 31;
 		var bombBottom = elBomb.offsetTop + 10;
-		var windowHeight = window.innerHeight;
-		if ((bombLeft > conditionTrigger) && (bombLeft > conditionTrigger || bombTop > 0) && (bombTop >= 0 && bombBottom <= windowHeight)) { // if all of them true bomb move if one of them false bombs explodes
 
-			// timeoutBomb = setInterval(function() {},10)
+		if ((bombLeft > conditionTrigger) && (bombLeft > conditionTrigger || bombTop > 0) && (bombTop > 0 && bombBottom < (windowHeight - 1))) {
 
 			var randomBombSpeed = Math.ceil(Math.random() * level + 0.25);
 
 			elBomb.style.top = bombTop - wayBomb[directions] + "px";
 			elBomb.style.left = bombLeft - randomBombSpeed + "px";
 
-			var elBombAtPos = document.elementFromPoint(bombLeft, bombTop); //check position from top/left points
-			var elBombAtPosInverse = document.elementFromPoint(bombRight, bombBottom); //check position from inverse points
-			// is statement for player collision with bomb
-			if (elBombAtPos.classList.contains("head") || elBombAtPos.classList.contains("body") || elBombAtPosInverse.classList.contains("head") || elBombAtPosInverse.classList.contains("body")) { //check collision from multiple points
-				// console.log("Hey - I'm dead");
+			var elBombAtPos = document.elementFromPoint(bombLeft, bombTop);
+			var elBombAtPosInverse = document.elementFromPoint(bombRight, bombBottom);
+			var elBombAtPosInverseReverse = document.elementFromPoint(bombLeft, bombBottom);
+			var elBombAtPosInverseInverse = document.elementFromPoint(bombRight, bombTop);
+
+			// console.log("elBombAtPos:", elBombAtPos);
+			// console.log("elBombAtPosInverse:", elBombAtPosInverse);
+
+			if (elBombAtPos && (elBombAtPos.classList.contains("head") || elBombAtPos.classList.contains("body")) ||
+				(elBombAtPosInverse && (elBombAtPosInverse.classList.contains("head") || elBombAtPosInverse.classList.contains("body") || elBombAtPosInverse.classList.contains("body")))) {
 				explodeBomb(bombs[i]);
 				bombs[i].remove();
 				removeLife();
+			} if (elBombAtPos.classList.contains("arrow") || elBombAtPosInverse.classList.contains("arrow") || elBombAtPosInverseInverse.classList.contains("arrow") || elBombAtPosInverseReverse.classList.contains("arrow")) {
+				console.log("bomb explodes");
+				explodeBomb(bombs[i]);
+				bombs[i].remove();
+				arrow[0].remove();
+				var elScore = document.getElementsByClassName('score')[0];
+				score++;
+				directions = Math.floor(Math.random() * 7);
+				hiddenScore++;
+				addLifeStore++;
+				elScore.innerHTML = "Your Score: " + score;
+				levelUp();
+				addLife();
+				clearInterval(timeoutArrow);
 			}
+
 		} else {
 			explodeBomb(bombs[i]);
 			bombs[i].remove();
-			var elScore = document.getElementsByClassName('score')[0]; /////  SCORE COUNTER =========
+			var elScore = document.getElementsByClassName('score')[0];
 			score++;
 			directions = Math.floor(Math.random() * 7);
 			hiddenScore++;
 			addLifeStore++;
 			elScore.innerHTML = "Your Score: " + score;
-			levelUp(); //call function
+			levelUp();
 			addLife();
 		}
 	}
@@ -576,7 +736,6 @@ function addBomb() {
 // end ============================================================================================
 // spawn randomly tanks ==============================================================================
 function spawnTanks() {
-	var windowHeight = window.innerHeight;
 	for (var i = 0; i < tanks.length; i++) {
 		randomPointY = (Math.floor(Math.random() * windowHeight));
 		// console.log("Value of WindowHeight is" + tanks[i].style.top);
@@ -593,6 +752,7 @@ function spawnTanks() {
 	timeoutBomb = setInterval(moveBomb, 10);
 }
 // end =============================================================================================
+
 // start game  || remove start bar || add tanks ====================================================
 function startGame() {
 	startBar.style.display = "none";
@@ -601,13 +761,21 @@ function startGame() {
 	for (var i = 0; i < tanks.length; i++) {
 		tanks[i].style.display = "block";
 	}
-}
-startBar.addEventListener('click', startGame);
-//end ================================================================================================
-function myLoadFunction() {
+
 	timeout = setInterval(move, 10);
 	document.addEventListener('keydown', keydown);
 	document.addEventListener('keyup', keyup);
+}
+startBar.addEventListener('click', startGame);
+//end ================================================================================================
+
+function myLoadFunction() {
+
+	windowHeight = window.innerHeight;
+	windowWidth = window.innerWidth;
+
+	console.log("w h " + windowHeight);
+	console.log("w w " + windowWidth);
 	scoreCount();
 	designLevel();
 	//hide tanks
